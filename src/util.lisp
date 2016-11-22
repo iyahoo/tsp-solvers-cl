62a468e7b2ed041cad51aebf7ace98266e3843b7
iyahoo <s1200191@gmail.com>
2016-11-22 14:32:28 +0900

    [Add] make graph

--- src/util.lisp
+++ src/util.lisp
@@ -38,3 +38,19 @@
 @export
 (defun read-random-state ()
   (read-file-from *random-state-path*))
+
+;; make graph
+(defmacro set-array (array i j num)
+  `(setf (aref ,array ,i ,j) ,num))
+
+(defun make-graph (n &rest argv)
+  (declare (ignore argv))
+  (let* ((nth (parse-integer n))
+         (adj-mat (make-array (list nth nth))))
+    (loop :for i :to (1- nth) :do
+      (loop :for j :from i :to (1- nth) :do
+        (if (= i j)
+            (set-array adj-mat i j 0)
+            (let ((random-number (1+ (random 9))))
+              (set-array adj-mat i j random-number)
+              (set-array adj-mat j i random-number)))))))
