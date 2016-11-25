f6b5c3e45378fb4e8b3109094d1723da65cedb57
masaponto <masaponto@gmail.com>
2016-11-26 00:52:21 +0900

    [Fix]

--- tsp_visualizer.py
+++ tsp_visualizer.py
@@ -22,7 +22,7 @@ def get_count_matrix(vertex_num, paths):
     return count_matrix
 
 
-def generate_graph_fig(vertex_num, out_file, ant_paths, generation):
+def generate_graph_fig(vertex_num, out_file, ant_paths, graph, generation):
 
     g = Graph(format='png')
 
@@ -32,33 +32,41 @@ def generate_graph_fig(vertex_num, out_file, ant_paths, generation):
         for j in range(i + 1, vertex_num):
             count = count_matrix[i][j]
             if count == 0:
-                g.edge(str(i), str(j), color="blue", penwidth="0.2")
+                g.edge(str(i), str(j),
+                       label=str(graph[i][j]), color="blue", penwidth="0.2")
             else:
-                g.edge(str(i), str(j), penwidth=str(count * 0.4))
+                g.edge(str(i), str(j),
+                       label=str(graph[i][j]),  penwidth=str(count * 0.5))
 
     g.body.append('label="Generation: %02d"' % generation)
     g.body.append('fontsize=20')
     g.render(out_file, cleanup=True)
 
 
-def generate(vertex_num, ant_num, csv_path, out_dir):
+def generate(vertex_num, ant_num, csv_path, graph_path, out_dir):
 
     ant_path_list = read_csv(csv_path)
+    graph = read_csv(graph_path)
+
     generation = len(ant_path_list) // ant_num
 
     for i in range(generation):
         generate_graph_fig(vertex_num,
-                           out_dir + '/%02d' % i, ant_path_list[i * 10: (i + 1) * 10], i)
+                           out_dir + '/%02d' % i,
+                           ant_path_list[i * 10: (i + 1) * 10],
+                           graph,
+                           i)
 
 
 def main():
     vertex_num = 10
     ant_num = 10
-    #csv_path = 'result/5_10_05_10_100_0_100.csv'
-    csv_path = 'result/5_10_095_10_10_0_100.csv'
-    out_dir = 'graph_02'
+    csv_path = 'data/5_10_05_10_100_0_100.csv'
+    #csv_path = 'data/5_10_095_10_10_0_100.csv'
+    graph_path = 'data/graph.csv'
+    out_dir = 'graph_01'
 
-    generate(vertex_num, ant_num, csv_path, out_dir)
+    generate(vertex_num, ant_num, csv_path, graph_path, out_dir)
 
 
 if __name__ == "__main__":
